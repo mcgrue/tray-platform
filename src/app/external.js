ac05e65f869dfb2563603a61a5d38df3d2f33d30
Ben McGraw <ben.mcgraw@gmail.com>
2022-05-17 19:49:56 -0700

    Fix tests, see the actions turn green?

--- src/app/external.js
+++ src/app/external.js
@@ -1,1 +1,5 @@
-function add(a,b) { return a + b; }
+function sum(a, b) {
+	return a + b;
+}
+
+module.exports = sum;
--- src/app/external.test.js
+++ src/app/external.test.js
@@ -1,1 +1,5 @@
-function add(a,b) { return a + b; }
+const sum = require('./external');
+
+test("A test of adding", () => {
+	expect(sum(2,2)).toBe(4);
+});

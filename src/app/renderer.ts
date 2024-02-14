1b933a6bfd390c12cef0ab3984c262666c13e6b6
Ben McGraw <ben.mcgraw@gmail.com>
2024-02-13 21:50:53 -0800

    making the sound demo 'more legal'

--- src/app/renderer.ts
+++ src/app/renderer.ts
@@ -1,6 +1,39 @@
 
+function playSound(id:string) {
+  var audio = document.getElementById(id) as any;
+  audio.play();
+}
 
+function create_audio(label:string, file:string) : HTMLAudioElement
+{
+  const button = document.createElement('button');
+  button.onclick = ()=>{playSound(label)};
+  button.innerText = label;
+  
+  const audio = document.createElement('audio');
+  audio.src = file;
+  audio.id = label;
 
+  button.appendChild(audio);
+
+  return button;
+}
+
+console.log("here I go, rendering again")
+
+try {
+  const root = document.getElementsByTagName('body')[0];
+  const audio1 = create_audio("audio 1", "./resources/achievement-pop.wav")
+  const audio2 = create_audio("audio 2", "./resources/sully-fanfare.ogg")
+  const audio3 = create_audio("audio 3", "./resources/you-did-something-yay.wav")
+
+  root.appendChild(audio1);
+  root.appendChild(audio2);
+  root.appendChild(audio3);
+} catch(e) {
+  debugger;
+  console.error(e);
+}
 
 window['ipcComms'].onShowAlert((value) => {
   alert(value)
